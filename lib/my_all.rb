e9768d82fd699c0a73e867004c61183af3773c26
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-27 17:15:54 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
-def my_all?(collection)
+#.all? checks if there is a false or nil element in the array.
 
+def my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i]) #or .push, in this case shove is being used.
+    i = i + 1 #i += does the same thing. Use this if it's easier for you.
+  end
+
+  if block_return_values.include?(false)
+    false
+  else
+    true
 end
